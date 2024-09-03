d8bbd7c39f2739d9a6d5d4f1139615161bda8ed1
Kizzie <110618945+Anonymous56Risk@users.noreply.github.com>
2024-09-03 19:11:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,6 +8,8 @@ import re
 import string
 import time
 
+import pyrogram.utils
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
